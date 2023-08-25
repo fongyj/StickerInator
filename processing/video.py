6223f2bbf94fe7d5eb49689bfbbcef367fd46007
Yih Jie <fongyj@hotmail.com>
2023-08-25 12:25:51 +0800

    Migrate to cv2

--- processing/video.py
+++ processing/video.py
@@ -1,4 +1,4 @@
-from moviepy.editor import VideoFileClip
+import cv2
 import os
 import subprocess
 import re
@@ -9,14 +9,15 @@ class VideoProcessor:
         self.file = file
         os.makedirs("temp", exist_ok=True)
         self.video_path = f"temp/{os.path.basename(file.file_path)}"
-        self.ffmpeg_path = os.environ.get("FFMPEG_PATH")
 
     async def get_video(self):
         await self.file.download_to_drive(custom_path=self.video_path)
-        self.video_file_clip = VideoFileClip(self.video_path)
+        self.cap = cv2.VideoCapture(self.video_path)
 
     def process_video(self, start_min=None, start_sec=None, crop_duration=None):
-        width, height = self.video_file_clip.w, self.video_file_clip.h
+        width = int(self.cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        height = int(self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        fps = int(self.cap.get(cv2.CAP_PROP_FPS))
         scale = 512 / max(width, height)
         new_width, new_height = int(width * scale), int(height * scale)
 
@@ -25,19 +26,34 @@ class VideoProcessor:
             os.path.dirname(self.video_path),
             os.path.splitext(os.path.basename(self.video_path))[0] + ".webm",
         )
+        fourcc = cv2.VideoWriter.fourcc(*"vp90")
+        out = cv2.VideoWriter(output_video_path, fourcc, fps, (new_width, new_height))
         if start_min:
-            command = f"{self.ffmpeg_path} -i {self.video_path} -c:v libvpx-vp9 -ss 00:{start_min}:{start_sec}00 -t 00:00:0{crop_duration}00 -crf 40 -an -vf scale={new_width}:{new_height} -v quiet -y {output_video_path}"
+            start_frame = int((start_min * 60 + start_sec) * fps)
+            end_frame = start_frame + int(crop_duration * fps)
         else:
-            command = f"{self.ffmpeg_path} -i {self.video_path} -c:v libvpx-vp9 -crf 40 -an -vf scale={new_width}:{new_height} -v quiet -y {output_video_path}"
-        subprocess.call(command, shell=True)
+            start_frame = 0
+            end_frame = int(self.cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        frame_idx = 0
+        while self.cap.isOpened():
+            ret, frame = self.cap.read()
+            if not ret:
+                break
+            if frame_idx >= start_frame or frame_idx <= end_frame:
+                frame = cv2.resize(frame, (new_width, new_height))
+                out.write(frame)
+            frame_idx += 1
+        self.cap.release()
+        out.release()
         video_bytes = open(output_video_path, "rb").read()
         os.remove(output_video_path)
-        self.video_file_clip.close()
         os.remove(self.video_path)
         return video_bytes
 
     def get_duration(self):
-        return self.video_file_clip.duration
+        frames = int(self.cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        fps = self.cap.get(cv2.CAP_PROP_FPS)
+        return round(frames / fps, 3)
 
 
 def parse_crop(crop: str, duration):
